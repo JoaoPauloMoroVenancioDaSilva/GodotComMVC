714ea331f8cc2a81559557288be2e10be379a31a
AdvanceControl <163237388+AdvanceControl@users.noreply.github.com>
2025-11-02 19:41:36 +0800

    Add bracket pair colorization for GDScript

--- modules/gdscript/editor/gdscript_highlighter.h
+++ modules/gdscript/editor/gdscript_highlighter.h
@@ -95,6 +95,8 @@ private:
 	Color string_name_color;
 	Color type_color;
 
+	PackedColorArray bracket_pair_colors;
+
 	enum CommentMarkerLevel {
 		COMMENT_MARKER_CRITICAL,
 		COMMENT_MARKER_WARNING,
@@ -104,7 +106,13 @@ private:
 	Color comment_marker_colors[COMMENT_MARKER_MAX];
 	HashMap<String, CommentMarkerLevel> comment_markers;
 
+	// Index 0: paren_level, Index 1: square_bracket_level, Index 2: curly_brace_level
+	HashMap<int, Vector<int>> bracket_level_cache;
+
 	void add_color_region(ColorRegion::Type p_type, const String &p_start_key, const String &p_end_key, const Color &p_color, bool p_line_only = false, bool p_r_prefix = false);
+
+protected:
+	virtual void _clear_highlighting_cache() override;
 
 public:
 	virtual void _update_cache() override;
